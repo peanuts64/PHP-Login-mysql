e0f053d7e19bb276314c29e2a48af58b1ba21e4c
asa <asaralvarado@gmail.com>
2022-11-25 17:08:46 -0800

    added navigation

--- login.php
+++ login.php
@@ -15,15 +15,24 @@ include 'includes/login.inc.php';
 $page = new Template();
 $format = new Format();
 $login = new Login();
+#creat a html form inputs lable => type
 $form_inputs = array(
 	'Username' => 'text', 
 	'Password' => 'password' 
 	);
+#dump the array into self validating html form function
 $page->form_new_list($form_inputs);
+#add the output from the function onto the output var
 $body = $page->form;
+# if the $input_flag is valid, check credientaisl with validate_login();
+#echo $page->input_flag;
 $body .= ($page->input_flag == 'TRUE' ? $login->validate_login($page->post_data_inputs, $page->input_flag) : 'Please fill out the form');
+#navigation button
 $page->nav_bar_links('index.php'  , 'Home');
-$page->nav_bar_drop_links_get('Nav', $page->nav_page_locations);
+#navigation drop menue
+$navigatoin_links = array('Log In' => 'login.php',
+			'README' => 'README.md');
+$page->nav_bar_drop_links_get('Nav', $navigatoin_links);
 $html = $page->tag_wrap('ul', $page->nav_bar_links);
 $html .= $body;
 $html = $page->tag_class_wrap('table', 'cellpadding="0" cellspacking="0"',     $html);
@@ -31,5 +40,4 @@ $html = $page->tag_class_wrap('div', 'class="invoice-box"', $html);
 $html =	$format->HTML($html);
 $page->display_page($title, $page->tag_wrap('style', $page->css), $html);
 (isset($page->post_data_inputs) ? print_r($page->post_data_inputs) : '' );
-echo $page->input_flag;
 
